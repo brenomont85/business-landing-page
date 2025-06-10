d8f6caeafc4236a1f76ce2f4fe5d3d10b1a19dd1
Breno Monteiro <brenomont85@gmail.com>
2025-06-10 15:18:33 -0300

    feat: adds backgroundSwitch

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ let index = 0;
 const carousel = document.getElementById('carousel');
 const slides = carousel.children.length;
 
+const bgImages = [
+  "assets/image1.png",
+  "assets/image2.png",
+  "assets/image3.png"
+];
+
+const hero = document.getElementById("hero");
+
+function switchBackground() {
+  index = (index + 1) % bgImages.length;
+  hero.style.backgroundImage = `url('${bgImages[index]}')`;
+}
+
+setInterval(switchBackground, 5000);
+
+
+
 function updateCarousel() {
   carousel.style.transform = `translateX(-${index * 100}%)`;
 }
